c17554fb2883860e872649233bf1a4fb8456287d
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2022-06-07 15:03:45 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+const cors = require('cors')
+
+const questions = {
+    '1' :{
+        'question' : 'Tell us about yourself.'
+    },
+    '2': {
+        'question' : 'This is question 2'
+    },
+    '3' : {
+        'question' : 'This is question 3'
+    }
+}
+
+app.use(cors())
+
+//this responds to the get request and sends index.html
+app.get('/', (request, response) => (
+    response.sendFile(__dirname + '/index.html')
+))
+
+app.get('/api/:number', (request, response)=>{
+    const questionNumber = request.params.number
+    if(questions[questionNumber]) {
+        response.json(questions[questionNumber])
+    } else {
+        response.json(questions['1'])
+    }
+})
+
+app.listen(process.env.port || PORT, ()=>(
+    console.log('listening on 8000')
+))
